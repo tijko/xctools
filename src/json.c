af123a4a3868aaa5acb57044c353c98fb77ec992
tim <konick781@gmail.com>
2018-09-28 11:32:47 -0400

    Adjust format (line 80)

--- src/json.c
+++ src/json.c
@@ -59,12 +59,13 @@ struct json_response *make_json_request(struct json_request *jreq)
     DBusMessage *msg = make_dbus_call(conn, &(jreq->dmsg));
 
     if (!msg || dbus_message_get_type(msg) == DBUS_MESSAGE_TYPE_ERROR) {
-        char *failed_msg;
-        DBUS_REQ_ARG(failed_msg, "<Destination=%s Path=%s Interface=%s Member=%s>",
+        char *err;
+        int id = jreq->id;
+        DBUS_REQ_ARG(err, "<Destination=%s Path=%s Interface=%s Member=%s>",
                      jreq->dmsg.destination, jreq->dmsg.path,
                      jreq->dmsg.interface, jreq->dmsg.member);
-        DBUS_BROKER_WARNING("response to <%d> request failed %s", jreq->id, failed_msg);
-        free(failed_msg); 
+        DBUS_BROKER_WARNING("response to <%d> request failed %s", id, err);
+        free(err);
         free(jrsp);
         return NULL;
     }
