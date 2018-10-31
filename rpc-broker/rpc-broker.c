5e16bef713358e57280b19d4e70209a632477049
tim <konick781@gmail.com>
2018-10-31 14:36:25 -0400

    Reading data from one end

--- rpc-broker/rpc-broker.c
+++ rpc-broker/rpc-broker.c
@@ -297,7 +297,7 @@ void run_rawdbus(struct dbus_broker_args *args)
 //                fcntl(srv, F_SETFL, O_NONBLOCK);
 //                fcntl(client, F_SETFL, O_NONBLOCK);
                 int sret = 1, cret = 1;
-                while (sret >= 0 || cret >= 0) {
+                while (sret > 0 || cret > 0) {
                     // client recv loop
                     printf("Client...\n");
                     cret = loop(client, srv, v4v_recv, send);
