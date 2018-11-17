a04d7138a76cbc7f941282696338d7d96ff647ad
tim <konick781@gmail.com>
2018-11-17 18:48:34 -0500

    Add extra check on verbosity

--- rpc-broker/src/policy.c
+++ rpc-broker/src/policy.c
@@ -185,9 +185,10 @@ DBusMessage *db_list(void)
 
     DBusMessage *vms = make_dbus_call(conn, &dmsg);
 
-    if (!vms && verbose_logging)
-        DBUS_BROKER_WARNING("DBus message return error <db-list> %s", "");
-    else if (dbus_message_get_type(vms) == DBUS_MESSAGE_TYPE_ERROR) {
+    if (!vms) {
+        if (verbose_logging)        
+            DBUS_BROKER_WARNING("DBus message return error <db-list> %s", "");
+    } else if (dbus_message_get_type(vms) == DBUS_MESSAGE_TYPE_ERROR) {
         if (verbose_logging)
             DBUS_BROKER_WARNING("DBus message return error <db-list> %s", "");
         dbus_message_unref(vms);
