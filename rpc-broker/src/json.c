00f5727f35b01a83e0bf65c1ff7e1443bb1821e6
tim <konick781@gmail.com>
2019-02-14 18:06:54 -0500

    Re-factor of json.c

--- rpc-broker/src/json.c
+++ rpc-broker/src/json.c
@@ -21,38 +21,47 @@
 
 struct json_response *init_jrsp(void)
 {
-    struct json_response *jrsp = calloc(1, sizeof *jrsp);
+    struct json_response *jrsp;
+    jrsp = calloc(1, sizeof *jrsp);
+    if (!jrsp)
+        DBUS_BROKER_ERROR("Calloc Failed!");
+
     jrsp->args = json_object_new_array();
-    snprintf(jrsp->type, JSON_REQ_ID_MAX, JSON_RESP);
+    memcpy(jrsp->type, JSON_RESP, JSON_REQ_ID_MAX);
 
     return jrsp;
 }
 
 struct json_response *make_json_request(struct json_request *jreq)
 {
-    struct json_response *jrsp = init_jrsp();
-    DBusConnection *conn = jreq->conn;
+    struct json_response *jrsp;
+    DBusConnection *conn;
+    DBusMessage *msg;
+    const char *busname;
+    char *err;
+
+    jrsp = init_jrsp();
+    conn = jreq->conn;
     dbus_connection_flush(conn);
     jrsp->id = jreq->id;
 
-    if (!strcmp(jreq->dmsg.member, "Hello")) {
+    if (jreq->dmsg.member && !strcmp(jreq->dmsg.member, "Hello")) {
 
-        const char *busname = dbus_bus_get_unique_name(conn);
+        busname = dbus_bus_get_unique_name(conn);
 
         if (!busname)
             DBUS_BROKER_ERROR("DBus refused busname");
 
         snprintf(jrsp->response_to, JSON_REQ_ID_MAX - 1, "%d", jreq->id);
-        snprintf(jrsp->arg_sig, DBUS_MAX_ARG_LEN - 1, "s");
+        memcpy(jrsp->arg_sig, "s", 2);
         json_object_array_add(jrsp->args, json_object_new_string(busname));
         return jrsp;
     }
 
     snprintf(jrsp->response_to, JSON_REQ_ID_MAX - 1, "%d", jreq->id);
-    DBusMessage *msg = make_dbus_call(conn, &(jreq->dmsg));
+    msg = make_dbus_call(conn, &(jreq->dmsg));
 
     if (!msg || dbus_message_get_type(msg) == DBUS_MESSAGE_TYPE_ERROR) {
-        char *err;
         DBUS_REQ_ARG(err, "<Destination=%s Path=%s Interface=%s Member=%s>",
                      jreq->dmsg.destination, jreq->dmsg.path,
                      jreq->dmsg.interface, jreq->dmsg.member);
@@ -78,9 +87,10 @@ struct json_response *make_json_request(struct json_request *jreq)
 static void append_dbus_message_arg(int type, int idx, void **args,
                                           struct json_object *jarg)
 {
-    // The "argument" array -> `void **args` will be set-up as an array of
-    // `struct arg` and these types won't need to be declared;
-    // (possibly add union-type in the `struct arg`)
+    // XXX todo: have the "argument" array -> `void **args`
+    //           be set-up as an array of `struct arg` 
+    //           and these types won't need to be declared
+    //      (possibly adding union-type in the `struct arg`)
 
     switch (type) {
 
@@ -99,13 +109,18 @@ static void append_dbus_message_arg(int type, int idx, void **args,
             break;
         }
 
+        case ('d'): {
+            double json_double = json_object_get_double(jarg);
+            args[idx] = malloc(sizeof(double));
+            memcpy(args[idx], (void *) &json_double, sizeof(double));
+            break;
+        }
+
         case ('s'): {
             const char *json_str = json_object_get_string(jarg);
-            args[idx] = malloc(strlen(json_str) + 1);
-            if (json_str)
-                memcpy(args[idx], (void *) json_str, strlen(json_str) + 1);
-            else
-                ((char *) args[idx])[0] = '\0';
+            if (!json_str)
+                json_str = "";
+            args[idx] = strdup(json_str);
             break;
         }
 
@@ -124,21 +139,26 @@ static void append_dbus_message_arg(int type, int idx, void **args,
 static const char *get_json_str_obj(struct json_object *jobj, char *field)
 {
     struct json_object *jfield;
+
     if (!json_object_object_get_ex(jobj, field, &jfield))
         return NULL;
+
     return strdup(json_object_get_string(jfield));
 }
 
 void load_json_response(DBusMessage *msg, struct json_response *jrsp)
 {
     DBusMessageIter iter, sub;
+    char *sig;
+    struct json_object *args;
+
     dbus_message_iter_init(msg, &iter);
 
-    char *sig = dbus_message_iter_get_signature(&iter);
+    sig = dbus_message_iter_get_signature(&iter);
     snprintf(jrsp->arg_sig, DBUS_MAX_ARG_LEN - 1, "%s", sig);
     dbus_free(sig);
 
-    struct json_object *args = jrsp->args;
+    args = jrsp->args;
 
     if (jrsp->arg_sig && jrsp->arg_sig[0] == 'a') {
 
@@ -169,10 +189,16 @@ void load_json_response(DBusMessage *msg, struct json_response *jrsp)
 static signed int parse_json_args(struct json_object *jarray,
                                   struct json_request *jreq)
 {
-    char *signature;
+    char *signature, *sigptr;
+    size_t array_length;
+    int i, jtype;
+    struct json_object *jarg;
 
+    // XXX supports the removal of network-daemon/slave
     if (!jreq->dmsg.destination && jreq->dmsg.type) {
         signature = malloc(3);
+        if (!signature)
+            DBUS_BROKER_ERROR("Malloc Failed!");
         snprintf(signature, 3, "uu");
     } else
         signature = dbus_introspect(jreq);
@@ -182,19 +208,18 @@ static signed int parse_json_args(struct json_object *jarray,
         return -1;
     }
 
-    memcpy(jreq->dmsg.arg_sig, signature, strlen(signature) + 1);
-    size_t array_length = json_object_array_length(jarray);
+    strncpy(jreq->dmsg.arg_sig, signature, strlen(signature) + 1);
+    array_length = json_object_array_length(jarray);
     jreq->dmsg.arg_number = array_length;
-    char *sigptr = signature;
-    int i;
+    sigptr = signature;
+
     for (i = 0; i < array_length; i++) {
 
-        struct json_object *jarg = json_object_array_get_idx(jarray, i);
-        int jtype = json_object_get_type(jarg);
+        jarg = json_object_array_get_idx(jarray, i);
+        jtype = json_object_get_type(jarg);
 
         if (jtype == json_type_null) {
-            jreq->dmsg.args[i] = malloc(sizeof(char) * 2);
-            ((char *) jreq->dmsg.args[i])[0] = '\0';
+            jreq->dmsg.args[i] = strdup(""); 
             continue;
         }
 
@@ -210,51 +235,58 @@ static signed int parse_json_args(struct json_object *jarray,
 
 struct json_request *convert_json_request(char *raw_json_req)
 {
-    struct json_object *jobj = json_tokener_parse(raw_json_req);
+    struct json_request *jreq;
+    struct json_object *jobj, *jarray, *jint;
+
+    jobj = json_tokener_parse(raw_json_req);
 
     if (!jobj) {
         DBUS_BROKER_WARNING("<Error parsing json-request> %s", raw_json_req);
         return NULL;
     }
 
-    struct json_request *jreq = malloc(sizeof *jreq);
+    jreq = malloc(sizeof *jreq);
+    if (!jreq) {
+        DBUS_BROKER_WARNING("<Malloc Failed! %s", "");
+        return NULL;
+    }
+
     jreq->dmsg.destination = get_json_str_obj(jobj, "destination");
+    // XXX supports the removal of network-daemon/slave
     if (!jreq->dmsg.destination) {
         jreq->dmsg.type = get_json_str_obj(jobj, "type");
     } else
         jreq->dmsg.type = NULL;
 
-    jreq->dmsg.interface = get_json_str_obj(jobj, "interface");
-    jreq->dmsg.path = get_json_str_obj(jobj, "path");
-    jreq->dmsg.member = get_json_str_obj(jobj, "method");
+    if (!(jreq->dmsg.interface = get_json_str_obj(jobj, "interface")) ||
+        !(jreq->dmsg.path = get_json_str_obj(jobj, "path"))           ||
+        !(jreq->dmsg.member = get_json_str_obj(jobj, "method")))
+        goto request_error;
 
     jreq->conn = create_dbus_connection();
-
-    struct json_object *jarray;
     json_object_object_get_ex(jobj, "args", &jarray);
 
-    if (!jarray) {
-        DBUS_BROKER_WARNING("<Error json-request> %s", raw_json_req);
-        free(jreq);
-        jreq = NULL;
-        goto put_jobj;
-    }
+    if (!jarray) 
+        goto request_error;
 
-    if (parse_json_args(jarray, jreq) < 0) {
-        DBUS_BROKER_WARNING("<Error json-request> %s", raw_json_req);
-        free(jreq);
-        jreq = NULL;
-        goto put_jobj;
-    }
+    if (parse_json_args(jarray, jreq) < 0) 
+        goto request_error;
 
-    struct json_object *jint;
     json_object_object_get_ex(jobj, "id", &jint);
     jreq->id = json_object_get_int(jint);
 
-put_jobj:
+    // json free's recursively on objects
     json_object_put(jobj);
 
     return jreq;
+
+request_error:
+
+    DBUS_BROKER_WARNING("<Error json-request> %s", raw_json_req);
+    free_json_request(jreq);
+    json_object_put(jobj);
+
+    return NULL;
 }
 
 void free_json_request(struct json_request *jreq)
@@ -282,7 +314,9 @@ void free_json_request(struct json_request *jreq)
 
 struct json_object *convert_dbus_response(struct json_response *jrsp)
 {
-    struct json_object *jobj = json_object_new_object();
+    struct json_object *jobj;
+
+    jobj = json_object_new_object();
     json_object_object_add(jobj, "id", json_object_new_int(jrsp->id));
     json_object_object_add(jobj, "type", json_object_new_string(jrsp->type));
 
