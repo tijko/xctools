2fc96d9832ab7f2560684b2dfc7978043230e222
Tim Konick <konick781@gmail.com>
2019-04-08 13:15:11 -0400

    Log the length for-each de-marshal

--- rpc-broker/src/msg.c
+++ rpc-broker/src/msg.c
@@ -372,15 +372,17 @@ int exchange(int rsock, int ssock, uint16_t domid, bool is_client)
         memcpy(&(partial[partial_head]), buf, rbytes); 
         partial_head += rbytes;
         len = dbus_message_demarshal_bytes_needed(partial, partial_head);
-        DBUS_BROKER_EVENT("De-Marshal %d", len);
-        if (len <= partial_head) {
+        DBUS_BROKER_EVENT("Read (%d) De-Marshal (%d) Partial (%d)", rbytes, len, partial_head);
+        if (len < 0)
+            DBUS_BROKER_EVENT("De-Marshal error %s", ""); 
+        else if (len <= partial_head) {
             DBUS_BROKER_EVENT("Sending Msg %s", "");
             if (convert_raw_dbus(&dmsg, partial, len) < 1)
                 return -1;
             if (is_request_allowed(&dmsg, is_client, domid) == false)
                 return -1;
-            total += len;
             send(ssock, partial, len, 0);
+            total += len;
             partial_head -= len;
             // implement a circular buffer :D (wow how long ago, things have come full circle...no pun intended)
             memmove(partial, &partial[len], partial_head);  
